1ed5a700990465f2becb919e4227754655d1d151
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-07-30 08:49:05 +0200

    Fixed Update User

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -13,20 +13,21 @@ RE_USERNAME = re.compile(r"^[A-Za-z0-9\_\-\.äöüÄÖÜ]{3,50}$")
 
 
 @router.get("/", response_model=UserSchema)
-async def get_user(data=Depends(get_data)):
-    return data.get("user")
+async def get_user(data=Depends(get_data), db: DB = Depends(database_dependency)):
+    user: User = await db.first(select(User).filter_by(id=data["user"]["id"]))
+    return UserSchema(**{"id": user.id, "username": user.username})
 
 
 @router.put("/", response_model=UserSchema)
 async def update_user(updated_user: UpdateUser, data=Depends(get_data), db: DB = Depends(database_dependency)):
-    if await db.exists(select(User).filter_by(username=updated_user.username)):
-        raise HTTPException(409, "Username already used")
     user: User = await db.first(select(User).filter_by(id=data["user"]["id"]))
     if user.username == updated_user.username:
         updated_user.username = None
-    else:
+    elif updated_user.username:
         if not RE_USERNAME.match(updated_user.username):
             raise HTTPException(400, "Username doesn't match Regex")
+        if await db.exists(select(User).filter_by(username=updated_user.username)):
+            raise HTTPException(409, "Username already used")
     if not updated_user.username and not updated_user.password:
         raise HTTPException(400, "Nothing Changed")
     if not user:
